d288096b9f0c9d422336464caa913cf5568b737a
jimmylu1 <jimmy88lu@gmail.com>
2019-04-25 15:54:26 -0700

    Debounce and Added Initial Data Pull to Render

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,6 +2,7 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
+import _ from 'lodash'
 
 var handleVideoSearch = (q) => {
   //TODO:  Write an asynchronous action to handle a video search!
@@ -10,12 +11,12 @@ var handleVideoSearch = (q) => {
     query: q
   };
 
-  return (dispatch) => {
+  return _.debounce((dispatch) => {
     searchYouTube(options, (videos) => {
       dispatch(changeVideoList(videos));
       dispatch(changeVideo(videos[0]));
     });
-  }
+  }, 200);
 };
 
 export default handleVideoSearch;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
+import handleSearchChange from './actions/search.js'
 //TODO: Import the Provider component from 'react-redux' here!
 
 //TODO: Use the Provider component to make your store available to
@@ -14,5 +15,6 @@ ReactDOM.render(
   <Provider store={store}>
     <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />
   </Provider>,
-  document.getElementById('app')
+  document.getElementById('app'),
+  () => handleSearchChange('inception')(store.dispatch)
 );
